17f946f0d04c7d870343ed157289cf03f5a45131
Frnbrz <frnbrz@gmail.com>
2023-05-25 23:51:16 +0200

    feature get fact and catimage

--- projects/03-prueba-tecnica/src/pages/App.jsx
+++ projects/03-prueba-tecnica/src/pages/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "./App.css";
+import { getImage } from "../services/image.service";
+import { extractFirstWord } from "../utils/utils";
+import { useState } from "react";
+import { getFact } from "../services/facts.service";
+import image from "../mocks/download.jpg";
 
 function App() {
-  return <div>Problem</div>;
+  const [catFact, setCatFact] = useState("");
+  const getOneFact = async () => {
+    try {
+      const { fact } = await getFact();
+      setCatFact(extractFirstWord(fact));
+      console.log(catFact);
+    } catch (error) {}
+  };
+
+  const handleClick = () => {
+    getOneFact();
+  };
+
+  return (
+    <main className="main">
+      <h1>app</h1>
+      <button onClick={handleClick}>get fact</button>
+      <p>
+        <strong>fact is: </strong>
+        <span>{catFact === "" ? `t` : catFact}</span>
+      </p>
+      <img src={image} alt="image from cat api" />
+    </main>
+  );
 }
 
 export default App;
